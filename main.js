99b3a8a5647dba8c99378e00e37be60ac04077ff
Satyam Shah <satyamshah444@gmail.com>
2026-04-28 12:23:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.129.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 // ─── SPLASH SCREEN CONTROLLER ─────────────────────────────────────────────────
 const splash = document.getElementById('splash-screen');
